7f233568804ef9fb30371df40bdaa8160676f82b
Gazzz07 <124935058+gazzz07@users.noreply.github.com>
2024-12-12 22:50:08 +0000

    update

--- Chars/main.cpp
+++ Chars/main.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     char quotedLetter { 'a' };
-    char unquotedNumber { 97 };
+    char unquotedNumber {97};
 
     std::cout << "This is printed with single quotes: " << quotedLetter << "\nThis number (without quotes) prints the same char: " << unquotedNumber << "\n";
 }
